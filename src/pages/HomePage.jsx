8bd0eb0ce83c7a7f67574a3e1b1926608ce396af
Sarvesh Patil <sarveshspatil99@gmail.com>
2026-04-17 09:11:10 +0530

    Fetch data from the backend

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,24 @@
+import { useEffect, useState } from "react";
 import { Header } from "../components/Header";
-import { products } from "../../starting-code/data/products";
+import axios from "axios";
+// import { products } from "../../starting-code/data/products";
 import "./HomePage.css";
 
 export const HomePage = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/api/products");
+        setProducts(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getData();
+  }, []);
+
   return (
     <>
       <title>Ecommerce Project</title>
@@ -31,7 +47,7 @@ export const HomePage = () => {
                   </div>
                 </div>
 
-                <div className="product-price">{`$ ${(product.priceCents / 100).toFixed(2)}`}</div>
+                <div className="product-price">{`$${(product.priceCents / 100).toFixed(2)}`}</div>
 
                 <div className="product-quantity-container">
                   <select>
@@ -61,104 +77,6 @@ export const HomePage = () => {
               </div>
             );
           })}
-
-          {/* <div className="product-container">
-            <div className="product-image-container">
-              <img
-                className="product-image"
-                src="images/products/intermediate-composite-basketball.jpg"
-              />
-            </div>
-
-            <div className="product-name limit-text-to-2-lines">
-              Intermediate Size Basketball
-            </div>
-
-            <div className="product-rating-container">
-              <img
-                className="product-rating-stars"
-                src="images/ratings/rating-40.png"
-              />
-              <div className="product-rating-count link-primary">127</div>
-            </div>
-
-            <div className="product-price">$20.95</div>
-
-            <div className="product-quantity-container">
-              <select>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
-              </select>
-            </div>
-
-            <div className="product-spacer"></div>
-
-            <div className="added-to-cart">
-              <img src="images/icons/checkmark.png" />
-              Added
-            </div>
-
-            <button className="add-to-cart-button button-primary">
-              Add to Cart
-            </button>
-          </div>
-
-          <div className="product-container">
-            <div className="product-image-container">
-              <img
-                className="product-image"
-                src="images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg"
-              />
-            </div>
-
-            <div className="product-name limit-text-to-2-lines">
-              Adults Plain Cotton T-Shirt - 2 Pack
-            </div>
-
-            <div className="product-rating-container">
-              <img
-                className="product-rating-stars"
-                src="images/ratings/rating-45.png"
-              />
-              <div className="product-rating-count link-primary">56</div>
-            </div>
-
-            <div className="product-price">$7.99</div>
-
-            <div className="product-quantity-container">
-              <select>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
-              </select>
-            </div>
-
-            <div className="product-spacer"></div>
-
-            <div className="added-to-cart">
-              <img src="images/icons/checkmark.png" />
-              Added
-            </div>
-
-            <button className="add-to-cart-button button-primary">
-              Add to Cart
-            </button>
-          </div> */}
         </div>
       </div>
     </>
