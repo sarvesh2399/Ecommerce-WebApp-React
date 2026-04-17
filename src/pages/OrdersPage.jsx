2b2645634035fcc9e52f0a338db167c342c16e35
Sarvesh Patil <sarveshspatil99@gmail.com>
2026-04-17 12:26:02 +0530

    Fix Orders Page Issue

--- src/pages/OrdersPage.jsx
+++ src/pages/OrdersPage.jsx
@@ -54,40 +54,43 @@ export const OrdersPage = ({ cart }) => {
                 <div className="order-details-grid">
                   {order.products.map((orderProduct) => {
                     return (
-                      <>
-                        <Fragment key={orderProduct.product.id}>
-                          <div className="product-image-container">
-                            <img src={orderProduct.product.image} />
-                          </div>
+                      <Fragment key={orderProduct.product.id}>
+                        <div className="product-image-container">
+                          <img src={orderProduct.product.image} />
+                        </div>
 
-                          <div className="product-details">
-                            <div className="product-name">
-                              {orderProduct.product.name}
-                            </div>
-                            <div className="product-delivery-date">
-                              Arriving on: {dayjs(orderProduct.estimatedDeliveryTimeMs).format("MMMM D")}
-                            </div>
-                            <div className="product-quantity">Quantity: {orderProduct.quantity}</div>
-                            <button className="buy-again-button button-primary">
-                              <img
-                                className="buy-again-icon"
-                                src="images/icons/buy-again.png"
-                              />
-                              <span className="buy-again-message">
-                                Add to Cart
-                              </span>
+                        <div className="product-details">
+                          <div className="product-name">
+                            {orderProduct.product.name}
+                          </div>
+                          <div className="product-delivery-date">
+                            Arriving on:{" "}
+                            {dayjs(orderProduct.estimatedDeliveryTimeMs).format(
+                              "MMMM D",
+                            )}
+                          </div>
+                          <div className="product-quantity">
+                            Quantity: {orderProduct.quantity}
+                          </div>
+                          <button className="buy-again-button button-primary">
+                            <img
+                              className="buy-again-icon"
+                              src="images/icons/buy-again.png"
+                            />
+                            <span className="buy-again-message">
+                              Add to Cart
+                            </span>
+                          </button>
+                        </div>
+
+                        <div className="product-actions">
+                          <Link to="/tracking">
+                            <button className="track-package-button button-secondary">
+                              Track package
                             </button>
-                          </div>
-
-                          <div className="product-actions">
-                            <Link to="/tracking">
-                              <button className="track-package-button button-secondary">
-                                Track package
-                              </button>
-                            </Link>
-                          </div>
-                        </Fragment>
-                      </>
+                          </Link>
+                        </div>
+                      </Fragment>
                     );
                   })}
                 </div>
